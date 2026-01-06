e1b829b9104448f84e32ec14e68fa4b8cfa421f7
msoyabcode <mohdsoyab0785@gmail.com>
2026-01-06 22:35:23 +0530

    Make counter-app mobile responsive and clean code

--- src/App.jsx
+++ src/App.jsx
@@ -1,57 +1,48 @@
-import React from 'react'
-import { useState } from 'react'
+import React, { useState } from "react";
 
 const App = () => {
+  const [count, setCount] = useState(0);
 
-  const [count, setCount] = useState(0)
-
-  const increase = () =>{
-    setCount(count+1)
-  }
-
-  const decrease = () =>{
-    if(count>0){
-    setCount(count-1)
-    }
-  }
-
-  const reset = () =>{
-    setCount(0)
-  }
+  const increase = () => setCount(count + 1);
+  const decrease = () => count > 0 && setCount(count - 1);
+  const reset = () => setCount(0);
 
   return (
-  <div className="flex items-center justify-center min-h-screen bg-amber-50">
-    <div className="bg-amber-100 p-10 rounded-lg shadow-lg">
-      
-      <h1 className="text-5xl font-bold underline text-center mb-5">
-        Counter App
-      </h1>
+    <div className="flex items-center justify-center min-h-screen bg-amber-50 px-4">
+      <div className="bg-amber-100 p-6 md:p-10 rounded-lg shadow-lg w-full max-w-md">
+        <h1 className="text-4xl sm:text-5xl font-bold underline text-center mb-5">
+          Counter App
+        </h1>
 
-      <h1 className="text-6xl font-light text-center mb-10">{count}</h1>
+        <h1 className="text-5xl sm:text-6xl font-light text-center mb-8">
+          {count}
+        </h1>
 
-      <div className="flex justify-center gap-9 text-3xl">
-        <button 
-          onClick={increase} 
-          className="bg-green-500 text-white px-6 py-3 rounded hover:bg-green-600" >
-          Increase
-        </button>
+        <div className="flex flex-col sm:flex-row flex-wrap justify-center gap-4 sm:gap-6 text-xl sm:text-3xl">
+          <button
+            onClick={increase}
+            className="bg-green-500 text-white px-6 py-3 rounded active:bg-green-600 w-full sm:w-auto"
+          >
+            Increase
+          </button>
 
-        <button 
-          onClick={decrease} 
-          className="bg-red-500 text-white px-6 py-3 rounded hover:bg-red-600"   >
-          Decrease
-        </button>
+          <button
+            onClick={decrease}
+            className="bg-red-500 text-white px-6 py-3 rounded active:bg-red-600 w-full sm:w-auto"
+          >
+            Decrease
+          </button>
 
-        <button 
-          onClick={reset} 
-          className="bg-gray-500 text-white px-6 py-3 rounded hover:bg-gray-600"  >
-          Reset
-        </button>
+          <button
+            onClick={reset}
+            className="bg-gray-500 text-white px-6 py-3 rounded active:bg-gray-600 w-full sm:w-auto"
+          >
+            Reset
+          </button>
+        </div>
       </div>
     </div>
-  </div>
-)
+  );
+};
 
-}
-
-export default App
+export default App;
